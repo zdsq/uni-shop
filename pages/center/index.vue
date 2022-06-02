3c5b1ef59f54515659af949d6bc9736c4efe37b1
ZFY <487801780@qq.com>
2022-06-02 22:17:28 +0800

    初始化项目完成，创建一个空白项目

---
+++ pages/center/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		<view>个人中心</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<view>
+			首页
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
